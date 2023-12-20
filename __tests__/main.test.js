920491b0a984e711b089165b7d8e608546004dc9
Noelia Ruiz Martínez <nrm1977@gmail.com>
2023-12-20 06:13:33 +0100

    Improve test coverage

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -4,37 +4,37 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 const main = require('../src/main')
+const { getRepositoryId } = require('../src/get-repository-id')
 
 // Mock the GitHub Actions core library
 const infoMock = jest.spyOn(core, 'info').mockImplementation()
 const getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
 const setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
 const setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
-
-// Mock the action's main function
-const runMock = jest.spyOn(main, 'run').mockImplementation(id => {
-  if (!id) {
-    core.setFailed('ID is undefined')
-  }
-  core.setOutput('repoId', `${id}`)
-})
+jest.mock('../src/get-repository-id', () => ({
+  getRepositoryId: jest.fn().mockResolvedValue('some-id')
+}))
 
 describe('action', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
-  it('sets the repoId output', async () => {
-    // Mock the action's inputs
-    await main.run('some-id')
-    expect(runMock).toHaveReturned()
-    expect(setOutputMock).toHaveBeenCalledWith('repoId', expect.any(String))
+  it('calls getRepositoryId ', async () => {
+    await main.run()
+	expect(getRepositoryId).toHaveBeenCalled()
   })
 
-  it('fails setting the repoId', async () => {
-    // Mock the action's inputs
-    await main.run(undefined)
-    expect(runMock).toHaveReturned()
-    expect(setFailedMock).toHaveBeenCalledWith(expect.any(String))
+  it('calls setOutput ', async () => {
+	  getRepositoryId.mockResolvedValue('some-id')
+    await main.run()
+	expect(setOutputMock).toHaveBeenCalledWith('repo-id', 'some-id')
   })
+
+it('calls setFailed ', async () => {
+	  getRepositoryId.mockResolvedValue(undefined)
+    await main.run()
+	expect(setFailedMock).toHaveBeenCalled()
+  })
+
 })
