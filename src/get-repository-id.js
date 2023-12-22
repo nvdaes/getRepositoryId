d40ee56cee9e50fc77a1f83504422c80d03a833f
Noelia Ruiz Martínez <nrm1977@gmail.com>
2023-12-22 20:51:39 +0100

    Fixes

--- src/get-repository-id.js
+++ src/get-repository-id.js
@@ -2,7 +2,7 @@
  * Function to get the ID of the specified repo
  */
 const core = require('@actions/core')
-const { graphql } = require("@octokit/graphql");
+const { graphql } = require('@octokit/graphql')
 const { inputHelper } = require('./input-helper')
 
 async function getRepositoryId() {
@@ -13,12 +13,12 @@ async function getRepositoryId() {
       id
     }
   }`
-  
+
   const graphqlWithAuth = graphql.defaults({
-  headers: {
-    authorization: `token `${token}`,
-  },
-});
+    headers: {
+      authorization: `token ${token}`
+    }
+  })
   const result = await graphqlWithAuth(query, variables)
   const repoId = result.repository.id
   return repoId
