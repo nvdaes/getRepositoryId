06e82d09999b9f1495edd168ec514987118567e8
Noelia Ruiz Martínez <nrm1977@gmail.com>
2023-12-22 20:38:46 +0100

    Test with octokit/graphql

--- src/get-repository-id.js
+++ src/get-repository-id.js
@@ -2,21 +2,24 @@
  * Function to get the ID of the specified repo
  */
 const core = require('@actions/core')
-const github = require('@actions/github')
+const { graphql } = require("@octokit/graphql");
 const { inputHelper } = require('./input-helper')
 
 async function getRepositoryId() {
   const token = core.getInput('token')
-  const octokit = github.getOctokit(token, {
-    userAgent: 'getRepositoryIdVersion1'
-  })
   const variables = inputHelper()
   const query = `query($owner:String!, $name:String!) {
     repository(owner:$owner, name:$name){
       id
     }
   }`
-  const result = await octokit.graphql(query, variables)
+  
+  const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token `${token}`,
+  },
+});
+  const result = await graphqlWithAuth(query, variables)
   const repoId = result.repository.id
   return repoId
 }
